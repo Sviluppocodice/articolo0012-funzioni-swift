bc735b0e30ddb6533f13ef91387cf7b0b4a342ec
SviluppoCodice <51750512+Sviluppocodice@users.noreply.github.com>
2019-07-26 17:29:13 +0200

    Add files via upload

---
+++ Funzioni_Swift/Funzioni.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func sommaDueNumeri (_ num1: Int, _ num2: Int) -> Int {
+    
+    return num1 + num2
+}
+
+func sottraiDueNumeri () {
+    
+    let a = 1
+    let b = 2
+    
+    let c = a - b
+    print(c)
+}
+
+var somma = sommaDueNumeri(3, 4)
+somma += 3
+print(somma)
+
+//sommaDueNumeri()
+//sottraiDueNumeri()
+
+
